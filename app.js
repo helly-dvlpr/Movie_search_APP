4fbdb5d338879217b3036811f4e51a28495703ae
helly <helly.dvlpr21@gmail.com>
2020-05-10 04:47:43 +0000

    API search for movies

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+app.set("view engine","ejs");
+
+app.get("/", function(req, res){
+		res.render("search");
+		});
+
+app.get("/results",function(req,res){
+	// res.send("hello, it works!!");
+	var query = req.query.searchterm;
+	var url = "http://www.omdbapi.com/?s="+query+"&apikey=thewdb"
+	request(url,function(error,response,body){
+		if(!error && response.statusCode == 200){
+			var results = JSON.parse(body);
+			// res.send(results["Search"][0]["Title"]);
+			res.render("results",{results:results});
+		}
+	});
+})
+
+
+app.listen(3000,function(){
+	console.log("server has started..");
+})
